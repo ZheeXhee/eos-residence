de7a281c123bd1d6848bfc3cf5d16043c6e7fcde
Mingkang0 <98515527+Mingkang0@users.noreply.github.com>
2025-06-21 21:42:31 +0800

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
+ output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
